f371f1e57151ce13844554ea4e66017392f09098
Hồ Huy Hoàng <hoangho1147@gmail.com>
2024-05-03 17:05:06 +0700

    Add test

--- layouts/Main.tsx
+++ layouts/Main.tsx
@@ -59,7 +59,7 @@ export default function MainLayout({children, title, metaData, mainMenu, footerM
 					// @ts-ignore
 								client-code='KA-1172557-25'
 					// @ts-ignore
-								src='https://static.katalon.com/libs/traffic-agent/v1/traffic-agent.min.js'>
+								src='https://static.katalon.com/libs/traffic-agent/v1/traffic-agent.min.js?v=1'>
 
 				</script>
 			</Head>
