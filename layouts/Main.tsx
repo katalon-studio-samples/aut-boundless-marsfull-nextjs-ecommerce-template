994e08420afb54575e6fc8aa3c5d38fd769be263
tuananht <tuan.tran@katalon.com>
2025-08-25 16:54:25 +0700

    Change clientcode

--- layouts/Main.tsx
+++ layouts/Main.tsx
@@ -61,13 +61,13 @@ export default function MainLayout({children, title, metaData, mainMenu, footerM
 					// @ts-ignore
 					async
 					// @ts-ignore
-					src='https://static.staging.katalon.com/libs/traffic-agent/v1/traffic-agent.min.js'
+					src='https://static.katalon.com/libs/traffic-agent/v1/traffic-agent.min.js'
 					// @ts-ignore
-					client-code='KA-1940145-012'>
+					client-code='KA-1281120-14'>
 				</script>
 				<script
 					// @ts-ignore
-					src='https://static.staging.katalon.com/libs/traffic-agent/v1/truetest-sdk.min.js'>
+					src='https://static.katalon.com/libs/traffic-agent/v1/truetest-sdk.min.js'>
 				</script>
 			</Head>
 			<AlertWidget />
