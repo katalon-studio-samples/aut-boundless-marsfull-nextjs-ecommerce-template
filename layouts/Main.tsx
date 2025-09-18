6cd88d129cac0882edbc169c0c97d83484847f0e
laiphamkat <116550309+laiphamkat@users.noreply.github.com>
2025-09-18 16:32:04 +0700

    Update Main.tsx

--- layouts/Main.tsx
+++ layouts/Main.tsx
@@ -61,13 +61,13 @@ export default function MainLayout({children, title, metaData, mainMenu, footerM
 					// @ts-ignore
 					async
 					// @ts-ignore
-					src='https://static.staging.katalon.com/libs/traffic-agent/v1/traffic-agent.min.js?t=2'
+					src='https://static.katalon.com/libs/traffic-agent/v1/traffic-agent.min.js?t=3'
 					// @ts-ignore
-					client-code='KA-1940145-012'>
+					client-code='KA-1155367-02'>
 				</script>
 				<script
 					// @ts-ignore
-					src='https://static.staging.katalon.com/libs/traffic-agent/v1/truetest-sdk.min.js'>
+					src='https://static.katalon.com/libs/traffic-agent/v1/truetest-sdk.min.js'>
 				</script>
 			</Head>
 			<AlertWidget />
