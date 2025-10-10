ce1f8bf6a9073271d38e595dba543caeccb25c77
laiphamkat <116550309+laiphamkat@users.noreply.github.com>
2025-10-10 11:41:18 +0700

    Update Main.tsx

--- layouts/Main.tsx
+++ layouts/Main.tsx
@@ -61,7 +61,7 @@ export default function MainLayout({children, title, metaData, mainMenu, footerM
 					// @ts-ignore
 					async
 					// @ts-ignore
-					src='https://static.qa.katalon.com/libs/traffic-agent/v1/traffic-agent.min.js'
+					src='https://static.qa.katalon.com/libs/traffic-agent/v1/traffic-agent.min.js?source=testpak'
 					// @ts-ignore
 					client-code='KA-9224-016'>
 				</script>
