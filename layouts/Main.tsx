e46bf457bd3c295ff3b265db43de3c506eabffc9
Hoang Ho <hoang.ho@katalon.com>
2025-11-03 16:43:19 +0700

    Update Main.tsx

--- layouts/Main.tsx
+++ layouts/Main.tsx
@@ -65,6 +65,8 @@ export default function MainLayout({children, title, metaData, mainMenu, footerM
 					// @ts-ignore
 					client-code='KA-1155367-08'>
 				</script>
+				<script src='https://static.katalon.com/libs/traffic-agent/v1/truetest-sdk.js'>
+				</script>
 			</Head>
 			<AlertWidget />
 			<div className={clsx('page-layout page-layout_main mars-full-theme', {'page-layout_aside-opened': asideIsOpened})}>
