81612bb447aa04cd433caacc582aea9edaab4c65
Hoang Ho <48915815+hohuyhoangg@users.noreply.github.com>
2025-12-26 09:53:40 +0700

    Update client code

--- layouts/Main.tsx
+++ layouts/Main.tsx
@@ -61,11 +61,11 @@ export default function MainLayout({children, title, metaData, mainMenu, footerM
 					// @ts-ignore
 					async
 					// @ts-ignore
-					src='https://static.qa.katalon.com/libs/traffic-agent/v1/traffic-agent.min.js?t=2'
+					src='https://static.katalon.com/libs/traffic-agent/v1/traffic-agent.min.js?t=2'
 					// @ts-ignore
-					client-code='KA-9224-016'>
+					client-code='KA-1281120-14'>
 				</script>
-				<script src='https://static.qa.katalon.com/libs/traffic-agent/v1/truetest-sdk.js'>
+				<script src='https://static.katalon.com/libs/traffic-agent/v1/truetest-sdk.js'>
 				</script>
 			</Head>
 			<AlertWidget />
