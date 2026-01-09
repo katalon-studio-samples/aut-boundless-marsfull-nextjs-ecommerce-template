cc1098e0bcb657f54b495f976d864b4149a45e17
Hoang Ho <48915815+hohuyhoangg@users.noreply.github.com>
2026-01-09 13:27:41 +0700

    Update traffic agent script source URL

--- pages/checkout/[[...slug]].tsx
+++ pages/checkout/[[...slug]].tsx
@@ -99,7 +99,7 @@ export default function CheckoutPage() {
 				<meta name='robots' content='noindex' />
 				<script
 					// @ts-ignore
-					src='https://static.staging.katalon.com/libs/traffic-agent/v1/truetest-sdk.min.js'>
+					src='https://static.katalon.com/libs/traffic-agent/v1/truetest-sdk.min.js'>
 				</script>
 			</Head>
 			<div>
